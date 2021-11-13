e651a13e06c5f3531167f0a2f64fcc28b012d9db
Hardik Sachan <sachanhardik@gmail.com>
2021-11-13 10:50:10 +0530

    leetcode: array: #1672 - Richest Customer Wealth

---
+++ src/main/kotlin/com/example/concepts/arrays/assignments/p1672_richest_customer_wealth/Solution.kt
@@ -0,0 +1,21 @@
+package com.example.concepts.arrays.assignments.p1672_richest_customer_wealth
+
+class Solution {
+    fun maximumWealth(accounts: Array<IntArray>): Int {
+        var maxWealth = 0
+        accounts.map { it.sum() }.forEach {
+            if (it > maxWealth) maxWealth = it
+        }
+        return maxWealth
+    }
+}
+
+fun main() {
+    println(
+        Solution().maximumWealth(arrayOf(
+            intArrayOf(1, 5),
+            intArrayOf(7, 3),
+            intArrayOf(3, 5)
+        ))
+    )
+}
