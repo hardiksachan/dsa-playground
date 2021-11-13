7b2cb40946b7ea79414b842c86fd532a1a8ad845
Hardik Sachan <sachanhardik@gmail.com>
2021-11-13 17:12:36 +0530

    leetcode: array: #1773 - Count Items Matching a Rule

---
+++ src/main/kotlin/com/example/concepts/arrays/assignments/p1773_count_items_matching_a_rule/Solution.kt
@@ -0,0 +1,35 @@
+package com.example.concepts.arrays.assignments.p1773_count_items_matching_a_rule
+
+import kotlin.IllegalArgumentException
+
+class Solution {
+    fun countMatches(
+        items: List<List<String>>,
+        ruleKey: String,
+        ruleValue: String
+    ): Int {
+        var ans = 0
+        val ruleIdx = when (ruleKey) {
+            "type" -> 0
+            "color" -> 1
+            "name" -> 2
+            else -> throw IllegalArgumentException()
+        }
+        items.forEach { item ->
+            if (item[ruleIdx] == ruleValue) ans++
+        }
+        return ans
+    }
+}
+
+fun main () {
+    println(
+        Solution().countMatches(
+            listOf(
+                listOf("phone","blue","pixel"),
+                listOf("computer","silver","lenovo"),
+                listOf("phone","gold","iphone")
+            ), "color", "silver"
+        )
+    )
+}
