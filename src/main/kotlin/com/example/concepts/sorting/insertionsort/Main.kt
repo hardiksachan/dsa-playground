cddc64c1a31df13c9b730b445ae244019d65f7c7
Hardik Sachan <sachanhardik@gmail.com>
2021-11-30 12:51:18 +0530

    insertion sort lec done

---
+++ src/main/kotlin/com/example/concepts/sorting/insertionsort/Main.kt
@@ -0,0 +1,19 @@
+package com.example.concepts.sorting.insertionsort
+
+fun main() {
+    val arr = intArrayOf(1, 2, 5, 4, 3, 6, 7, 8, 9, -5)
+    insertionSort(arr)
+    println(arr.contentToString())
+}
+
+fun insertionSort(arr: IntArray) { 
+    for (i in 0 until arr.lastIndex) {
+        for (j in (i+1) downTo 1) {
+            if (arr[j] >= arr[j-1]) break // this line decides asc or desc
+
+            val t = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = t
+        }
+    }
+}
